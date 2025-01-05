8130276ab76e318c5b91a6e8a23c7a68f0693c6d
shson <sunghyun7895@naver.com>
2025-01-05 15:16:16 +0900

    손성현 플랩 무료참가 서브 알림 개발 v2.6(파티셔닝) - 잡 끝나지 않는 오류 개선(poolSize 조정이 아닌 taskExectuor 세팅 제외로 개선)

--- src/main/java/com/sunghyun/football/config/TaskExecutorConfig.java
+++ src/main/java/com/sunghyun/football/config/TaskExecutorConfig.java
@@ -7,9 +7,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class TaskExecutorConfig {
-    private static int CORE_POOL_SIZE= 10;
-    private static int MAX_POOL_SIZE = 10;
-//    private static int QUEUE_CAPACITY = 100;
+    private static int CORE_POOL_SIZE= 4;
+    private static int MAX_POOL_SIZE = 8;
+    private static int QUEUE_CAPACITY = 10;
     private static String THREAD_NAME_PREFIX = "multi-thread-";
 
     @Bean
@@ -18,11 +18,11 @@ public class TaskExecutorConfig {
 
         executor.setCorePoolSize(CORE_POOL_SIZE);
         executor.setMaxPoolSize(MAX_POOL_SIZE);
-//        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
         executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
         executor.setWaitForTasksToCompleteOnShutdown(Boolean.TRUE);
         executor.initialize();
-//        executor.setAllowCoreThreadTimeOut(true);
+        executor.setAllowCoreThreadTimeOut(true);
 
         return executor;
     }
