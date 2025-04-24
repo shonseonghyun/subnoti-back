90ab1626aa66c451b11f5cd5270d75e3ba6f3941
손성현 <sunghyun7895@naver.com>
2025-04-24 21:15:16 +0900

    타임존 설정

--- src/main/java/com/sunghyun/football/FootballApplication.java
+++ src/main/java/com/sunghyun/football/FootballApplication.java
@@ -1,10 +1,13 @@
 package com.sunghyun.football;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.TimeZone;
 
 @Slf4j
 @EnableFeignClients
@@ -14,5 +17,10 @@ public class FootballApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FootballApplication.class, args);
+	}
+
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
