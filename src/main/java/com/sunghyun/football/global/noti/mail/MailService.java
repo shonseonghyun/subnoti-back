ee037dc5f7b026f17fcf9e9855267eb1a6e8340b
손성현 <sunghyun7895@naver.com>
2025-05-20 15:09:07 +0900

    플랩 무료참가 서브 알림 개발(멀티스레드) v2.6 - 이메일 발송 비동기 개발

--- src/main/java/com/sunghyun/football/global/noti/mail/MailService.java
+++ src/main/java/com/sunghyun/football/global/noti/mail/MailService.java
@@ -1,20 +1,21 @@
 package com.sunghyun.football.global.noti.mail;
 
-import com.sunghyun.football.global.noti.NotiProcessor;
 import com.sunghyun.football.global.noti.NotiSendReqDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MailNotiProcessor extends NotiProcessor {
+public class MailService{
     private final JavaMailSender javaMailSender;
 
-    public void doNoti(NotiSendReqDto notiSendReqDto){
+    @Async
+    public void send(NotiSendReqDto notiSendReqDto){
         log.info("[{}] 메일 발송",notiSendReqDto.getSubject());
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(notiSendReqDto.getEmail());
@@ -22,5 +23,4 @@ public class MailNotiProcessor extends NotiProcessor {
         simpleMailMessage.setText(notiSendReqDto.getContent());
         javaMailSender.send(simpleMailMessage);
     }
-
 }
